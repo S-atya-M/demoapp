19d499aa771c911061e91ab510236aeeb1c53fbf
satyam <dsatyam034@gmail.com>
2023-11-30 19:08:54 +0530

    added try catch

--- app.py
+++ app.py
@@ -18,19 +18,24 @@ st.sidebar.header('User Input')
 
 #create a funtion to get the user Input
 def get_input():
-  start_date = st.sidebar.text_input("Start Date", "2010-01-02")
-  end_date = st.sidebar.text_input("End Date", "2020-08-04")
-  stock_symbol = st.sidebar.text_input("Stock Symbol", "AMZN")
-  return start_date, end_date, stock_symbol
- 
+    start_date = st.sidebar.text_input("Start Date", "2010-01-02")
+    end_date = st.sidebar.text_input("End Date", "2020-08-04")
+    stock_symbol = st.sidebar.text_input("Stock Symbol", "AAPL")  # Default symbol is AAPL (Apple Inc.)
+    return start_date, end_date, stock_symbol
 
-
-#Get the users Input
+# Get the user's input
 startDate, endDate, symbol = get_input()
-#get data on this ticker
-tickerData = yf.Ticker(symbol)
-#get the historical prices for this ticker
-tickerDf = tickerData.history(period='id', start= startDate, end= endDate)
-#open high   low close 	volume 	dividends 	stock splits
-st.line_chart(tickerDf.Close)
-st.line_chart(tickerDf.Volume)
+
+try:
+    # Get data on the ticker
+    tickerData = yf.Ticker(symbol)
+    
+    # Get the historical prices for the ticker
+    tickerDf = tickerData.history(period='1d', start=startDate, end=endDate)
+    
+    # Display the closing price and volume in line charts
+    st.line_chart(tickerDf.Close)
+    st.line_chart(tickerDf.Volume)
+    
+except Exception as e:
+    st.error(f"An error occurred: {e}")
